657811f8346e534837e301f9051f18b37c0eb73b
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-04-18 13:57:15 +0800

    Add Barrier item

--- src/main/java/io/github/bluereflega/barrier/BarrierMod.java
+++ src/main/java/io/github/bluereflega/barrier/BarrierMod.java
@@ -1,7 +1,13 @@
 package io.github.bluereflega.barrier;
 
+import io.github.bluereflega.barrier.item.ModItems;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +17,13 @@ public class BarrierMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Barrier Mod");
 
+	public static final Item BARRIER = new Item(new QuiltItemSettings());
+
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+
+		// ModItems.registerModItems();
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "barrier"), BARRIER);
 	}
 }
