ee7d1595ee4188549f4f17bc152724b7ee0e3b73
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-04-19 23:34:19 +0800

    Use dust particle for barrier effect

--- src/main/java/io/github/bluereflega/barrier/item/custom/BarrierItem.java
+++ src/main/java/io/github/bluereflega/barrier/item/custom/BarrierItem.java
@@ -4,13 +4,14 @@ import io.github.bluereflega.barrier.util.math.SphericalCoordinateHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.joml.Vector3f;
 
 public class BarrierItem extends Item {
 	public BarrierItem(Settings settings) {
@@ -56,8 +57,7 @@ public class BarrierItem extends Item {
 				double azimuth = maxAzimuth * ((double) (azimuthStep) / azimuthSteps);
 
 				world.addParticle(
-					// TODO: use dust particle instead of end rod particle
-					ParticleTypes.END_ROD,
+					new DustParticleEffect(new Vector3f(0.3f, 0.6f, 1.0f), 1.0f),
 					playerX + SphericalCoordinateHelper.toCartX(radius, zenith, azimuth),
 					playerY + SphericalCoordinateHelper.toCartY(radius, zenith),
 					playerZ + SphericalCoordinateHelper.toCartZ(radius, zenith, azimuth),
